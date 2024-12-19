e95ae0e876caa6f78286fc15e5907640d76ef70d
InimitablePioneer <seungwonpioneer@gmail.com>
2024-12-19 13:59:28 +0900

    DEC19 at work via WIN

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -4,25 +4,68 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 
+    private final JdbcTemplate template;
+
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
+        this.template = new JdbcTemplate(dataSource);
+
+    }
+
     @Override
     public Item save(Item item) {
-        return null;
+        String sql ="insert into item(item_name,price,quantity) values(?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(connection ->{
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, item.getItemName());
+            ps.setInt(2, item.getPrice());
+            ps.setInt(3, item.getQuantity());
+            return ps;
+        },keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
     }
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-
+        String sql = "update item set item_name=?,price=?,quantity=? where id=?";
+        template.update(sql,
+                updateParam.getItemName(),
+                updateParam.getPrice(),
+                updateParam.getQuantity(),
+                itemId);
     }
 
     @Override
     public Optional<Item> findById(Long id) {
+        String sql = "select id,item_name,price,quantity from item where id=?";
+        try {
+            template.queryForObject(sql,)
+        } catch () {
+        }
+
+
         return Optional.empty();
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return (rs,rowNum) -> {
+
+        }
     }
 
     @Override
