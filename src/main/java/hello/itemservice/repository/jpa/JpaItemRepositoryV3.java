d4cc3216d8a99a252a4dae0e8636c7a5fca5e81d
InimitablePioneer <seungwonpioneer@gmail.com>
2025-02-26 13:27:19 +0900

    FEB26 at work via WIN

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,6 +1,7 @@
 package hello.itemservice.repository.jpa;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
 import hello.itemservice.domain.QItem;
@@ -16,12 +17,16 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @Repository
 @Transactional
 public class JpaItemRepositoryV3 implements ItemRepository {
 
     private final EntityManager em;
     private final JPAQueryFactory query;
+
+    private QItem item = QItem.item;
 
     public JpaItemRepositoryV3(EntityManager em) {
         this.em = em;
@@ -50,7 +55,31 @@ public class JpaItemRepositoryV3 implements ItemRepository {
 
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
-        return List.of();
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        List<Item> result = query.select(item)
+                .from(item)
+                .where(likeItemName(itemName), maxPrice(maxPrice))
+                .fetch();
+
+        return result;
+
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%"+itemName+"%");
+        }
+        return null;
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
     }
 
     //@Override
@@ -71,4 +100,5 @@ public class JpaItemRepositoryV3 implements ItemRepository {
         List<Item> result = query.select(item).from(item).where(builder).fetch();
         return result;
     }
+
 }
