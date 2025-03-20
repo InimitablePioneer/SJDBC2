cc7fe1539c55ef7c5c71ae59d8aeb47432276e9e
InimitablePioneer <seungwonpioneer@gmail.com>
2025-03-20 13:28:42 +0900

    Update

---
+++ src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
@@ -0,0 +1,11 @@
+package hello.itemservice.repository.v2;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemQueryRepositoryV2 {
+
+    private final JPAQueryFactory query;
+
+}
