430d37505ff810860181dca44fb84b163c300f56
InimitablePioneer <seungwonpioneer@gmail.com>
2025-03-21 13:24:26 +0900

    update

--- src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
+++ src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
@@ -1,11 +1,32 @@
 package hello.itemservice.repository.v2;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+
+import static hello.itemservice.domain.QItem.item;
 
 @Repository
 public class ItemQueryRepositoryV2 {
 
     private final JPAQueryFactory query;
+
+    public ItemQueryRepositoryV2(EntityManager em) {
+        this.query = new JPAQueryFactory(em);
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+
+        return null;
+    }
+
+
+
 
 }
